e48445321a52f8e7b757873e9b491c049c106f30
jacksalah <jack.salah@yahoo.com>
2018-04-24 01:30:39 +0200

    perviousStep

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-		System.out.println("jack do it ");
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
